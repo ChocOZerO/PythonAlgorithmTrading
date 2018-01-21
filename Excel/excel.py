6520e8e69846f1022066fd30dc8029797b88476c
Taehyeon Jake Lee <nayalth@gmail.com>
2018-01-22 00:12:39 +0900

    셀에 색깔 입히기

--- Excel/excel.py
+++ Excel/excel.py
@@ -4,5 +4,8 @@ excel = win32com.client.Dispatch("Excel.Application")
 excel.Visible = True
 wb = excel.Workbooks.Open('d:\\IT\\ProgrammingStudy\\PythonAlgorithmTrading\\Excel\\test.xlsx')
 ws = wb.ActiveSheet
-print(ws.Cells(1, 1).Value)
+ws.Cells(2, 1).Value = "Python"
+ws.Cells(2, 2).Value = "is"
+ws.Range("C2").Value = "good"
+ws.Range("C2").Interior.ColorIndex = 10
 excel.Quit()
