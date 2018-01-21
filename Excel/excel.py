be6801d1bf2484bb537b05ffff0a2626eba0bb37
Taehyeon Jake Lee <nayalth@gmail.com>
2018-01-22 00:04:34 +0900

    파일 저장

--- Excel/excel.py
+++ Excel/excel.py
@@ -4,3 +4,6 @@ excel = win32com.client.Dispatch("Excel.Application")
 excel.Visible = True
 wb = excel.Workbooks.Add()
 ws = wb.Worksheets("Sheet1")
+ws.Cells(1, 1).Value = "hello world"
+wb.SaveAs('d:\\IT\\ProgrammingStudy\\PythonAlgorithmTrading\\Excel\\test.xlsx')
+excel.Quit()
