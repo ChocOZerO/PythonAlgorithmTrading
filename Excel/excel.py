c90b6f7d422c891b71f58a1a0bb172683d60ae66
Taehyeon Jake Lee <nayalth@gmail.com>
2018-01-22 00:08:43 +0900

    파일 읽기

--- Excel/excel.py
+++ Excel/excel.py
@@ -2,8 +2,7 @@ import win32com.client
 
 excel = win32com.client.Dispatch("Excel.Application")
 excel.Visible = True
-wb = excel.Workbooks.Add()
-ws = wb.Worksheets("Sheet1")
-ws.Cells(1, 1).Value = "hello world"
-wb.SaveAs('d:\\IT\\ProgrammingStudy\\PythonAlgorithmTrading\\Excel\\test.xlsx')
+wb = excel.Workbooks.Open('d:\\IT\\ProgrammingStudy\\PythonAlgorithmTrading\\Excel\\test.xlsx')
+ws = wb.ActiveSheet
+print(ws.Cells(1, 1).Value)
 excel.Quit()
